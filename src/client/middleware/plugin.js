9e35e850a1eb6c9f8e69486a6e3beb45e96f9abd
blackmiaool <blackmiaool@gmail.com>
2016-11-15 19:12:19 +0800

    fix: fix plugin system

--- src/client/middleware/plugin.js
+++ src/client/middleware/plugin.js
@@ -1,42 +1,4 @@
 import plugin from 'chat-room-plugin';
-import jQuery from 'jquery';
-
-const $ = jQuery;
-
-function findUserMessage(userName) {
-    if (!userName) {
-        return;
-    }
-    let fullMatch = false;
-    const match = userName.match(/^"([\s\S]*)"$/);
-    if (match) {
-        userName = match[1];
-        fullMatch = true;
-    }
-    const $names = $('.message-list-item').find('.message-username');
-    let $item;
-    for (let i = $names.length - 1; i >= 0; i--) {
-        const thisName = $names.eq(i).text();
-        if (fullMatch) {
-            if (thisName === userName) {
-                $item = $names.eq(i).parents('.message-list-item');
-                break;
-            }
-        } else {
-            if (thisName.indexOf(userName) !== -1) {
-                $item = $names.eq(i).parents('.message-list-item');
-                break;
-            }
-        }
-    }
-
-    if ($item) {
-        $item.avatar = $item.find('.avatar-image,.avatar-text');
-        $item.direction = $item.find('.message-self').length ? 'right' : 'left';
-    }
-
-    return $item;
-}
 
 function getPluginMessageInfo(message) {
     let {
@@ -71,12 +33,6 @@ function getPluginMessageInfo(message) {
     };
     return ret;
 }
-
-plugin.init({
-    findUserMessage,
-    getPluginMessageInfo,
-    jQuery,
-});
 
 function pluginMiddleware(message) {
     const pluginMessageInfo = getPluginMessageInfo(message);
--- src/client/webPc/main/chat/message/plugin.js
+++ src/client/webPc/main/chat/message/plugin.js
@@ -4,6 +4,84 @@ import plugin from 'chat-room-plugin';
 
 import base from './base';
 
+const $ = jQuery;
+
+function findUserMessage(userName) {
+    if (!userName) {
+        return;
+    }
+    let fullMatch = false;
+    const match = userName.match(/^"([\s\S]*)"$/);
+    if (match) {
+        userName = match[1];
+        fullMatch = true;
+    }
+    const $names = $('.message-list-item').find('.message-username');
+    let $item;
+    for (let i = $names.length - 1; i >= 0; i--) {
+        const thisName = $names.eq(i).text();
+        if (fullMatch) {
+            if (thisName === userName) {
+                $item = $names.eq(i).parents('.message-list-item');
+                break;
+            }
+        } else {
+            if (thisName.indexOf(userName) !== -1) {
+                $item = $names.eq(i).parents('.message-list-item');
+                break;
+            }
+        }
+    }
+
+    if ($item) {
+        $item.avatar = $item.find('.avatar-image,.avatar-text');
+        $item.direction = $item.find('.message-self').length ? 'right' : 'left';
+    }
+
+    return $item;
+}
+
+function getPluginMessageInfo(message) {
+    let {
+        content,
+    } = message;
+
+    const match = content.trim().match(/^([a-zA-Z0-9_-]+)\s*\(([\s\S]*)\)\s*;?\s*$/);
+
+    const name = match && match[1];
+    if (!name) {
+        return;
+    }
+
+    const typeInfo = plugin.messageList[name];
+    if (!typeInfo) {
+        return;
+    }
+    const {
+        showBase,
+        process,
+    } = typeInfo;
+
+    if (process) {
+        content = process(message);
+    } else {
+        content = match[2];
+    }
+    const ret = {
+        name,
+        content,
+        showBase,
+    };
+    return ret;
+}
+
+plugin.init({
+    findUserMessage,
+    getPluginMessageInfo,
+    jQuery,
+});
+
+
 class PluginMessage extends React.Component {
     static propTypes = {
         name: PropTypes.string.isRequired,
@@ -42,10 +120,11 @@ const pluginMessage = {
     shouldRender: messageType => messageType === 'plugin',
     render: (message, me) => {
         const showBase = message.get('pluginMessageInfo').get('showBase');
+        const pmessage = <PluginMessage name={message.get('pluginMessageInfo').get('name')} content={message.get('pluginMessageInfo').get('content')} isNew={message.get('isNew')} />;
         if (showBase) {
-            return base(pluginMessage, message, me);
+            return base(pmessage, message, me);
         } else {
-            return <PluginMessage name={message.get('pluginMessageInfo').get('name')} content={message.get('pluginMessageInfo').get('content')} isNew={message.get('isNew')} />;
+            return pmessage;
         }
     },
 };
