b9e234934c060ae4b2d52d0db89b9944d2ad1187
碎碎酱 <yinxin630@gmail.com>
2016-11-17 18:08:34 +0800

    feat: add remote-redux-devtools

--- src/client/store.js
+++ src/client/store.js
@@ -1,9 +1,10 @@
 import { createStore } from 'redux';
 import { combineReducers } from 'redux-immutable';
+import devToolsEnhancer from 'remote-redux-devtools';
 
 import pc from './reducer/pc';
 import mobile from './reducer/mobile';
 import user from './reducer/user';
 
 const reducers = combineReducers({ pc, mobile, user });
-export default createStore(reducers);
+export default createStore(reducers, devToolsEnhancer({ realtime: true, port: 8000 }));
