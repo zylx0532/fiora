c1983b8fdda43fef5bb7c1316ac36f4856da166b
碎碎酱 <yinxin630@gmail.com>
2016-11-18 11:00:31 +0800

    feat: remote-redux-devtools conditional compilation

--- src/client/store.js
+++ src/client/store.js
@@ -1,10 +1,21 @@
 import { createStore } from 'redux';
 import { combineReducers } from 'redux-immutable';
-import devToolsEnhancer from 'remote-redux-devtools';
 
 import pc from './reducer/pc';
 import mobile from './reducer/mobile';
 import user from './reducer/user';
 
+let devToolsEnhancer = null;
+if (process.env.NODE_ENV === 'development') {
+    devToolsEnhancer = require('remote-redux-devtools');
+}
+
 const reducers = combineReducers({ pc, mobile, user });
-export default createStore(reducers, devToolsEnhancer({ realtime: true, port: 8000 }));
+let store = null;
+if (devToolsEnhancer) {
+    store = createStore(reducers, devToolsEnhancer({ realtime: true, port: 8000 }));
+}
+else {
+    store = createStore(reducers);
+}
+export default store;
