fbc3e57b2b9afad576480826cbd04fad2e277566
碎碎酱 <yinxin630@gmail.com>
2016-11-30 16:44:38 +0800

    fix: fix send image bug

--- src/client/webPc/main/chat/message/image.js
+++ src/client/webPc/main/chat/message/image.js
@@ -6,13 +6,14 @@ const imageMessage = {
     shouldRender: messageType => messageType === 'image',
     render: (message, me, scrollMessage) => {
         let img = null;
+        const content = message.get('content');
         return base(
             <div
                 className="image"
             >
                 <img
                     style={{ maxHeight: window.innerHeight > 800 ? 300 : 200 }}
-                    src={`${message.get('content')}?imageView/2/w/200/h/200`}
+                    src={`${content}${/^http/.test(content) ? '?imageView/2/w/200/h/200' : ''}`}
                     ref={i => img = i}
                     onLoad={scrollMessage}
                     onError={() => img.src = 'http://assets.suisuijiang.com/image_not_found.png?imageView2/2/w/250'}
