483f99bbd409e35d48c87dc8215ff3db1f405cb5
碎碎酱 <yinxin630@gmail.com>
2017-02-15 18:00:01 +0800

    feat: add antd

--- src/client/next/App.js
+++ src/client/next/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import { DatePicker } from 'antd';
+
+import 'normalize.css';
+import 'next/styles/app.less';
 
 class App extends Component {
     render() {
         return (
             <div>
-                <p>Hello Fiora 5.0</p>
+                <h1 className="title">Ant Design 组件测试</h1>
+                <DatePicker />
             </div>
         );
     }
