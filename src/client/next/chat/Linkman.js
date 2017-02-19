c94754e45a321b64544dc1df30efdd5a72200587
碎碎酱 <yinxin630@gmail.com>
2017-02-19 20:28:57 +0800

    feat: modify Header and Linkman style

--- src/client/next/chat/Linkman.js
+++ src/client/next/chat/Linkman.js
@@ -1,24 +1,36 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import Avatar from 'components/Avatar';
 
 class LinkManItem extends Component {
+    static propTypes = {
+        index: PropTypes.number.isRequired,
+        focus: PropTypes.bool,
+        onClick: PropTypes.func,
+    }
+    handleClick = () => {
+        const { index, onClick } = this.props;
+        onClick(index);
+    }
     render() {
+        const { focus } = this.props;
         return (
             <div
-                className="item"
-                onClick={this.handleUserListItemClick}
+                className={focus ? 'item focus' : 'item'}
+                onClick={this.handleClick}
             >
-                <Avatar
-                    className="avatar"
-                    width={40} height={40}
-                />
-                <div className="content">
-                    <div className="nick-time flex-row">
-                        <p>{ '碎碎酱' }</p>
-                        <p>{ '12:34' }</p>
-                    </div>
-                    <div className="text">
-                        <p>{ '呵呵呵的内容, 特别长的怎么办呢啊啊啊啊啊' }</p>
+                <div>
+                    <Avatar
+                        className="avatar"
+                        width={40} height={40}
+                    />
+                    <div className="content">
+                        <div className="nick-time flex-row">
+                            <p>{ '碎碎酱' }</p>
+                            <p>{ '12:34' }</p>
+                        </div>
+                        <div className="text">
+                            <p>{ '呵呵呵的内容, 特别长的怎么办呢啊啊啊啊啊' }</p>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -27,12 +39,31 @@ class LinkManItem extends Component {
 }
 
 class LinkMan extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            focusIndex: 0,
+        };
+    }
+    handleLinkmanClick = (index) => {
+        this.setState({ focusIndex: index });
+    }
     render() {
+        const { focusIndex } = this.state;
         return (
             <div className="linkman">
-                <LinkManItem />
-                <LinkManItem />
-                <LinkManItem />
+                <LinkManItem
+                    index={0} focus={focusIndex === 0}
+                    onClick={this.handleLinkmanClick}
+                />
+                <LinkManItem
+                    index={1} focus={focusIndex === 1}
+                    onClick={this.handleLinkmanClick}
+                />
+                <LinkManItem
+                    index={2} focus={focusIndex === 2}
+                    onClick={this.handleLinkmanClick}
+                />
             </div>
         );
     }
--- src/client/next/header/Header.js
+++ src/client/next/header/Header.js
@@ -1,49 +1,37 @@
 import React, { Component } from 'react';
-import { Layout, Dropdown, Menu, Input } from 'antd';
-import Avatar from 'components/Avatar';
+import { Layout, Input } from 'antd';
 
 import 'next/styles/header.less';
 
 const { Header } = Layout;
 const Search = Input.Search;
 
-function handleMenuClick({ key }) {
-    console.log(`click at ${key}`);
-}
-
-const menu = (
-    <Menu onClick={handleMenuClick}>
-        <Menu.Item key="1">
-            <p>个人设置</p>
-        </Menu.Item>
-        <Menu.Item key="2">
-            <p>退出登录</p>
-        </Menu.Item>
-    </Menu >
-);
-
 class AppHeader extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchInputFocus: false,
+        };
+    }
+    handleInputFocus = () => {
+        this.setState({ searchInputFocus: true });
+    }
+    handleInputBlur = () => {
+        console.log('blur');
+        this.setState({ searchInputFocus: false });
+    }
     render() {
+        const { searchInputFocus } = this.state;
         return (
             <Header className="header">
-                <div className="user">
-                    <Dropdown overlay={menu} placement="bottomCenter">
-                        <div>
-                            <Avatar width={44} height={44} />
-                            <p className="nick">碎碎酱</p>
-
-                        </div>
-                    </Dropdown>
+                <div className="logo">
+                    <img src={require('assets/images/logo.png')} alt="logo" />
                 </div>
                 <div className="function">
                     <Search
-                        placeholder="input search text"
-                        style={{ width: 200 }}
-                        onSearch={value => console.log(value)}
+                        className={searchInputFocus ? 'input-focus' : ''} placeholder="搜索用户 / 群组"
+                        onFocus={this.handleInputFocus} onBlur={this.handleInputBlur}
                     />
-                </div>
-                <div className="logo">
-                    <img src={require('assets/images/logo.png')} alt="logo" />
                 </div>
             </Header>
         );
